bdbfcd811e96116876b9a58311c496c448451638
Hugo <hugo.cridlig@esme.fr>
2024-03-04 13:36:58 +0100

    add cors to API

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 //create express app
 const express = require('express');
 const pool = require('./db'); // Import the database connection
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World denn,dedefrom express');
