754d59094477d8c9b209c52ace4007658c1cdded
Hugo <hugo.cridlig@esme.fr>
2024-03-11 16:33:20 +0100

    add react dom routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,81 +1,27 @@
-// App.js
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+import Signup from './components/SignUp';
+import Quiz from './components/QuizH';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import {Routes , Route} from 'react-router-dom';
 
-import React, { Component } from 'react';
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/quiz" element={<Quiz />} />
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: [],
-      currentQuestionIndex: 0,
-      userAnswers: [],
-    };
-  }
-
-  componentDidMount() {
-    // Effectuer une requête à votre API pour récupérer les questions
-    fetch('https://quiz-web-app-five.vercel.app/api/questions')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ questions: data });
-      })
-      .catch(error => console.error('Erreur lors de la récupération des questions', error));
-  }
-
-  render() {
-    const { questions, currentQuestionIndex } = this.state;
-  
-    if (questions.length === 0) {
-      return <p>Chargement des questions...</p>;
-    }
-  
-    const currentQuestion = questions[currentQuestionIndex];
-  
-    // Extract options from individual properties
-    const options = [currentQuestion.optiona, currentQuestion.optionb, currentQuestion.optionc, currentQuestion.optiond].filter(Boolean);
-  
-    // Check if options is an array
-    if (!Array.isArray(options) || options.length === 0) {
-      console.error('Invalid question data. Options should be an array.', currentQuestion);
-      return null; // or handle the error in another way
-    }
-  
-    return (
-      <div>
-        <h1>Quiz App</h1>
-        <p>{currentQuestion.question}</p>
-        <ul>
-          {options.map((option, index) => (
-            <li key={index}>
-              <label>
-                <input
-                  type="radio"
-                  value={option}
-                  onChange={() => this.handleAnswerChange(index)}
-                  checked={index === this.state.userAnswers[currentQuestionIndex]}
-                />
-                {option}
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  
-  
-
-  handleAnswerChange = (index) => {
-    const userAnswers = [...this.state.userAnswers];
-    userAnswers[this.state.currentQuestionIndex] = index;
-    this.setState({ userAnswers });
-  };
-
-  handleNextQuestion = () => {
-    this.setState((prevState) => ({
-      currentQuestionIndex: prevState.currentQuestionIndex + 1,
-    }));
-  };
+      </Routes>
+    </div>
+    
+    
+  );
 }
 
 export default App;
