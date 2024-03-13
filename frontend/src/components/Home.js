c669b6c17ec4945205ae5ca26d00c9efebc20c95
AlexD4V <alex4921@hotmail.fr>
2024-03-13 14:59:34 +0100

    print categories dinamiclly

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CategoriesPage = () => {
-  const categories = [
-    { id: 1, name: 'Sports', image: 'https://example.com/sports.jpg' },
-    { id: 2, name: 'Histoire', image: 'https://example.com/histoire.jpg' },
-    { id: 3, name: 'Musique', image: 'https://example.com/musique.jpg' },
-    // Ajoutez autant de catégories que vous voulez
-  ];
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://quiz-web-app-five.vercel.app/categories')
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
 
   return (
     <div>
       <h1>Choisissez une catégorie</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {categories.map((category) => (
-          <div key={category.id} style={{ margin: '1rem', textAlign: 'center' }}>
-            <img src={category.image} alt={category.name} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
-            <h2>{category.name}</h2>
+          <div key={category.idcat} style={{ flexBasis: '23%', margin: '1rem 0.5rem', textAlign: 'center' }}>
+            <img src={category.image} alt={category.title} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
+            <h2>{category.title}</h2>
+            <p>{category.description}</p>
             <button style={{ backgroundColor: '#4CAF50', color: '#fff', cursor: 'pointer', fontSize: '1rem', padding: '0.5rem 1rem', borderRadius: '5px', transition: 'background-color 0.3s ease' }}>Sélectionner</button>
           </div>
         ))}
