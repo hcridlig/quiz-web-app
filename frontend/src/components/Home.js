7c0ae3f3c342e10dd9541bb50020f59db2a57657
Hugo <hugo.cridlig@esme.fr>
2024-03-13 23:01:38 +0100

    redirection !token

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
+import Cookies from 'js-cookie';
 
 const CategoriesPage = () => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    const timer = setTimeout(() => {
+      if (!token) {
+        navigate('/signin');
+      } else {
+        setToken(token);
+        setLoading(false);
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   const categories = [
     { id: 1, name: 'Sports', image: 'https://example.com/sports.jpg' },
     { id: 2, name: 'Histoire', image: 'https://example.com/histoire.jpg' },
     { id: 3, name: 'Musique', image: 'https://example.com/musique.jpg' },
-    // Ajoutez autant de catégories que vous voulez
+    // Add as many categories as you want
   ];
+
+  if (loading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress /> {}
+      </div>
+    );
+  }
 
   return (
     <div>
