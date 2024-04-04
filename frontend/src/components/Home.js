cc509c0fe02591eeec76b8023b8f50cdd99f2392
Hugo <hugo.cridlig@esme.fr>
2024-04-04 08:49:58 +0200

    add the navigate to waiting room

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,27 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
-import Cookies from 'js-cookie';
 
 const CategoriesPage = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
   const [token, setToken] = useState('');
   const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
 
- 
   useEffect(() => {
-    const token = Cookies.get('token');
-    if (!token) {
-      navigate('/signin');
-    } else {
-      setToken(token);
-      setLoading(false);
-    }
-
     const fetchCategories = async () => {
       try {
-        const response = await fetch('https://quiz-web-app-five.vercel.app/categories', {
+        const response = await fetch(`${apiUrl}/categories`, {
           headers: {
             Authorization: `Bearer ${token}` // Include token in the Authorization header
           }
@@ -31,37 +22,46 @@ const CategoriesPage = () => {
         }
         const data = await response.json();
         setCategories(data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching categories:', error);
         // Handle error
+        setLoading(false);
       }
     };
 
     fetchCategories();
-  }, [token]);
+  }, []);
 
-
-  if (loading) {
-    return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <CircularProgress /> {}
-      </div>
-    );
-  }
+  const handleCategorySelect = (category) => {    
+    // Redirect to the waiting room
+    navigate('/waiting-room', { state: { category: category } });
+  };
 
   return (
     <div>
       <h1>Choisissez une catégorie</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        {categories.map((category) => (
-          <div key={category.idcat} style={{ flexBasis: '23%', margin: '1rem 0.5rem', textAlign: 'center' }}>
-            <img src={category.image} alt={category.title} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
-            <h2>{category.title}</h2>
-            <p>{category.description}</p>
-            <button style={{ backgroundColor: '#4CAF50', color: '#fff', cursor: 'pointer', fontSize: '1rem', padding: '0.5rem 1rem', borderRadius: '5px', transition: 'background-color 0.3s ease' }}>Sélectionner</button>
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          {categories.map((category) => (
+            <div key={category.idcat} style={{ flexBasis: '23%', margin: '1rem 0.5rem', textAlign: 'center' }}>
+              <img src={category.image} alt={category.title} style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
+              <h2>{category.title}</h2>
+              <p>{category.description}</p>
+              <button
+                style={{ backgroundColor: '#4CAF50', color: '#fff', cursor: 'pointer', fontSize: '1rem', padding: '0.5rem 1rem', borderRadius: '5px', transition: 'background-color 0.3s ease' }}
+                onClick={() => handleCategorySelect(category.title)}
+              >
+                Sélectionner
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
