caee35204f599ead26c4d3159afea995a622fab8
Hugo <hugo.cridlig@esme.fr>
2024-04-04 09:58:11 +0200

    add username and modified when connected

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
 const Navbar = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
   const [anchorEl, setAnchorEl] = useState(null);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [username, setUsername] = useState('');
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -14,34 +17,107 @@ const Navbar = () => {
     setAnchorEl(null);
   };
 
+  useEffect(() => {
+    const tokenCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('token='));
+
+    const checkAuthentication = async () => {
+      if (tokenCookie) {
+        try {
+          const response = await fetch(`${apiUrl}/users/get-player-name`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${getTokenFromCookie()}`,
+            },
+          });
+          if (response.ok) {
+            const data = await response.json();
+            setUsername(data.playerName);
+            setAuthenticated(true);
+          } else {
+            setAuthenticated(false);
+          }
+        } catch (error) {
+          console.error('Error checking authentication:', error);
+          setAuthenticated(false);
+        }
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+
+  const getTokenFromCookie = () => {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith('token=')) {
+        return cookie.substring('token='.length, cookie.length);
+      }
+    }
+    return null;
+  };
+
   return (
     <nav style={{ backgroundColor: 'rgba(230, 230, 230, 0.5)', display: 'flex', justifyContent: 'flex-end', padding: '1rem', position: 'fixed', top: 0, left: 0, right: 0 }}>
-      <ul style={{ listStyle: 'none', margin: 0, padding: 0, display: 'flex' }}>
-        <li><button style={{ backgroundColor: 'transparent', border: 'none', color: '#444', cursor: 'pointer', fontSize: '1rem', marginLeft: '1rem', padding: '0.5rem 1rem', transition: 'background-color 0.3s ease' }}>S'enregistrer</button></li>
-        <li><button style={{ backgroundColor: 'transparent', border: 'none', color: '#444', cursor: 'pointer', fontSize: '1rem', marginLeft: '1rem', padding: '0.5rem 1rem', transition: 'background-color 0.3s ease' }}>Se connecter</button></li>
-        <li><button style={{ backgroundColor: '#4CAF50', color: '#fff', cursor: 'pointer', fontSize: '1rem', marginLeft: '1rem', padding: '0.5rem 1rem', borderRadius: '5px', transition: 'background-color 0.3s ease' }}>Lancer un quizz</button></li>
-        <li>
-          <Button
-            variant="contained"
-            color="primary"
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-            style={{ marginLeft: '1rem' }}
-          >
-            Menu
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Option 1</MenuItem>
-            <MenuItem onClick={handleClose}>Option 2</MenuItem>
-            <MenuItem onClick={handleClose}>Option 3</MenuItem>
-          </Menu>
-        </li>
+      <ul style={{ listStyle: 'none', margin: 0, padding: 0, display: 'flex', alignItems: 'center' }}>
+        {authenticated ? (
+          <>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '1rem' }}>Bonjour {username}</span>
+            </li>
+            <li>
+              <Button
+                variant="contained"
+                color="primary"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                style={{ marginLeft: '1rem' }}
+              >
+                Menu
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Option 1</MenuItem>
+                <MenuItem onClick={handleClose}>Option 2</MenuItem>
+                <MenuItem onClick={handleClose}>Option 3</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Button
+                style={{ marginLeft: '1rem', display: authenticated ? 'none' : 'inline-block' }}
+                variant="contained"
+                color="primary"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                S'enregistrer
+              </Button>
+            </li>
+            <li>
+              <Button
+                style={{ marginLeft: '1rem', display: authenticated ? 'none' : 'inline-block' }}
+                variant="contained"
+                color="primary"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                Se connecter
+              </Button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
