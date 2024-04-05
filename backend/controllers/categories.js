2bfc511aead6affff2a06a0afc5629bce367c899
Hugo <hugo.cridlig@esme.fr>
2024-04-05 19:00:31 +0200

    order category by name

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -3,7 +3,11 @@ const { Category } = require('../models/categories');
 const categoriesController = {
     getAllCategories: async (req, res) => {
         try {
-          const users = await Category.findAll();
+          const users = await Category.findAll(
+            {
+              order: [['title', 'ASC']],
+            }
+          );
           res.json(users);
         } catch (error) {
           console.error(error);
