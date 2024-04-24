8ffca2f1d73094b25c90178c210f2bdca44f3fa6
AlexD4V <alex4921@hotmail.fr>
2024-04-24 17:00:02 +0200

    commit maj APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import Login from './components/Login';
 import Signup from './components/SignUp';
 import Quiz from './components/QuizH';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
+import Categories from './components/Home';
 //import WaitingRoom from './components/WaitingRoom';
 import Account from './components/EditUser';
 import AddQuestions from './components/AddQuestion';
 import UserManagement from './components/UserManagement';
 import AddUser from './components/AddUser';
+import Accueil from './components/Accueil';
+import Ranking from './components/Ranking';
+import GameHistory from './components/GameHistory';
 import { Routes , Route, useLocation } from 'react-router-dom';
+import { Category } from '@mui/icons-material';
 
 function App() {
   const location = useLocation();
@@ -22,7 +26,7 @@ function App() {
     <div>
       {!hideNavbar && <Navbar />}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Categories />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/game" element={<Quiz />} />
@@ -31,6 +35,9 @@ function App() {
         <Route path="/add-question" element={<AddQuestions />} />
         <Route path="/user-management" element={<UserManagement />} />
         <Route path="/add-user" element={<AddUser />} />
+        <Route path="/" element={<Accueil />} />
+        <Route path="/ranking" element={<Ranking />} />
+        <Route path="/history" element={<GameHistory />} />
       </Routes>
     </div>
   );
