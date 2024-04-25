d34b0125d347697c433de4baed75d534a932d5e8
Hugo <hugo.cridlig@esme.fr>
2024-04-25 15:23:29 +0200

    add result and wiating room

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,12 @@ import Signup from './components/SignUp';
 import Quiz from './components/QuizH';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-//import WaitingRoom from './components/WaitingRoom';
+import WaitingRoom from './components/WaitingRoom';
 import Account from './components/EditUser';
 import AddQuestions from './components/AddQuestion';
 import UserManagement from './components/UserManagement';
 import AddUser from './components/AddUser';
+import ResultPage from './components/ResultPage';
 import { Routes , Route, useLocation } from 'react-router-dom';
 
 function App() {
@@ -26,11 +27,12 @@ function App() {
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/game" element={<Quiz />} />
-        { /*<Route path="/waiting-room" element={<WaitingRoom />} />*/}
+        <Route path="/waiting-room/:roomId" element={<WaitingRoom />} />
         <Route path="/account" element={<Account />} />
         <Route path="/add-question" element={<AddQuestions />} />
         <Route path="/user-management" element={<UserManagement />} />
         <Route path="/add-user" element={<AddUser />} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </div>
   );
